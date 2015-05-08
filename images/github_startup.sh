3f0f371774938fecde2cf57d73a20821975d9937
Mark Ethan Trostler <mark@zzo.com>
2015-05-08 14:37:07 -0700

    typo

--- images/github_startup.sh
+++ images/github_startup.sh
@@ -4,7 +4,7 @@ curl -sL https://deb.nodesource.com/setup | sudo bash -
 apt-get install -y git nodejs libwww-perl build-essential
 git clone https://github.com/zzo/sivart
 cd sivart
-HOME=root npm install
+HOME=/root npm install
 if [[ $? -gt 0 ]] 
   then
     echo __ERROR__
