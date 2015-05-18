5dadd56763670ef71032ca47810c353858b7c13b
Mark Ethan Trostler <mark@zzo.com>
2015-05-18 15:02:26 -0700

    use 'slave' for image name

--- images/createSlaveImage.js
+++ images/createSlaveImage.js
@@ -2,4 +2,4 @@
 
 var ImageMaker = require('./ImageMaker');
 // This must match what's in ../instances/slave.json !!!
-ImageMaker('slave-snapshot', 'slave-4');
+ImageMaker('slave-snapshot', 'slave');
