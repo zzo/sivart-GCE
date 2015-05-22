4f028a149da805b9a6ad182e86a45a69f98a68cf
Mark Ethan Trostler <mark@zzo.com>
2015-05-22 15:55:03 -0700

    get instance name from command line for test

--- spec/Instance_get_spec.js
+++ spec/Instance_get_spec.js
@@ -1,8 +1,10 @@
 var Instance = require('../Instance');
+var instanceName = process.argv[3];
 
+console.log('IN: ' + instanceName);
 describe('Instance get', function() {
   it('gets an IP address', function(done) {
-    var instance = Instance.Factory('slave', 'x-1432304888869-0e78573f-e333-467c-8a0b-41f481ce1ff3');
+    var instance = Instance.Factory('slave', instanceName);
     instance.getIP(function(err, ip) {
       expect(err).toBeNull();
       expect(ip).toMatch(/\d+\.\d+\.\d+\.\d+/);
